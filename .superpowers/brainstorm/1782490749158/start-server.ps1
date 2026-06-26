2e3db8839ed3883386216db9da2e2e9540ccce01
DESKTOP-DS240P5\WSW <1564234202@qq.com>
2026-06-27 05:27:53 +0800

    chore: update .gitignore to block docs/ and hidden dirs, remove cached tracked files

--- .superpowers/brainstorm/1782490749158/start-server.ps1
+++
@@ -1,6 +0,0 @@
-$sessionDir = "C:\Users\WSW\AndroidStudioProjects\HealthDietPro\.superpowers\brainstorm\1782490749158"
-$env:BRAINSTORM_DIR = $sessionDir
-$env:BRAINSTORM_HOST = "127.0.0.1"
-$env:BRAINSTORM_URL_HOST = "localhost"
-Remove-Item "$sessionDir\state\server-stopped" -Force -ErrorAction SilentlyContinue
-node "C:\Users\WSW\.config\opencode\superpowers\skills\brainstorming\scripts\server.cjs"
