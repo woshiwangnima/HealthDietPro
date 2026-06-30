1a49e8719e46bdd6f9017f250d65a845764b6c4b
DESKTOP-DS240P5\WSW <1564234202@qq.com>
2026-07-01 01:44:36 +0800

    feat: add TabBinder + DefaultTabBinder

---
+++ app/src/main/java/com/woshiwangnima/healthdietpro/ui/widget/tab/TabBinder.kt
@@ -0,0 +1,46 @@
+package com.woshiwangnima.healthdietpro.ui.widget.tab
+
+import android.content.Context
+import android.graphics.Typeface
+import androidx.core.content.ContextCompat
+import com.woshiwangnima.healthdietpro.R
+
+interface TabBinder {
+    fun bind(item: TabItem, view: TabItemView, selected: Boolean, isCenter: Boolean)
+}
+
+object DefaultTabBinder : TabBinder {
+
+    override fun bind(item: TabItem, view: TabItemView, selected: Boolean, isCenter: Boolean) {
+        // Label
+        view.labelView.text = item.label
+        val labelPx = view.resources.getDimension(R.dimen.text_size_label)
+        view.labelView.setTextSize(android.util.TypedValue.COMPLEX_UNIT_PX, labelPx)
+        view.labelView.setTypeface(null, if (selected) Typeface.BOLD else Typeface.NORMAL)
+
+        // Icon
+        view.showIcon(item.icon != null)
+        if (item.icon != null) {
+            view.iconView.setImageResource(item.icon)
+            val tintColor = if (selected) R.color.primary else R.color.on_surface_variant
+            view.iconView.setColorFilter(ContextCompat.getColor(view.context, tintColor))
+        }
+
+        // Background + label color
+        val ctx = view.context
+        if (isCenter) {
+            view.background = ContextCompat.getDrawable(
+                ctx,
+                if (selected) R.drawable.bg_nav_center_selected else R.drawable.bg_nav_center
+            )
+            view.labelView.setTextColor(ContextCompat.getColor(ctx, R.color.primary))
+        } else {
+            view.background = ContextCompat.getDrawable(
+                ctx,
+                if (selected) R.drawable.tab_selected_bg else R.drawable.tab_default_bg
+            )
+            val labelColor = if (selected) R.color.primary else R.color.on_surface_variant
+            view.labelView.setTextColor(ContextCompat.getColor(ctx, labelColor))
+        }
+    }
+}
