5fc466585176624333b0c3433057cbefe4e46596
DESKTOP-DS240P5\WSW <1564234202@qq.com>
2026-07-01 01:52:49 +0800

    refactor: MainActivity nav uses ToggleBar + persists main tab

--- app/src/main/java/com/woshiwangnima/healthdietpro/MainActivity.kt
+++ app/src/main/java/com/woshiwangnima/healthdietpro/MainActivity.kt
@@ -1,19 +1,18 @@
 package com.woshiwangnima.healthdietpro
 
 import android.content.Intent
-import android.graphics.Typeface
 import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
-import androidx.core.content.ContextCompat
 import com.woshiwangnima.healthdietpro.base.BaseActivity
-import com.woshiwangnima.healthdietpro.config.NavConfig
 import com.woshiwangnima.healthdietpro.databinding.ActivityMainBinding
 import com.woshiwangnima.healthdietpro.model.prefs.AppPrefs
 import com.woshiwangnima.healthdietpro.ui.nutrition.NutritionFragment
 import com.woshiwangnima.healthdietpro.ui.profile.ProfileEditActivity
 import com.woshiwangnima.healthdietpro.ui.profile.ProfileFragment
 import com.woshiwangnima.healthdietpro.ui.record.RecordFragment
+import com.woshiwangnima.healthdietpro.ui.widget.tab.TabItem
+import com.woshiwangnima.healthdietpro.ui.widget.tab.ToggleBar
 import com.woshiwangnima.healthdietpro.util.applySystemBarInsets
 
 class MainActivity : BaseActivity() {
@@ -31,7 +30,7 @@ class MainActivity : BaseActivity() {
         ActivityResultContracts.StartActivityForResult()
     ) {
         AppPrefs.markFirstLaunchComplete(this)
-        switchTab(2)
+        binding.navBar.select(2)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,9 +40,16 @@ class MainActivity : BaseActivity() {
 
         binding.main.applySystemBarInsets()
 
-        setupNavSizing()
-        setupNavClickListeners()
-        switchTab(0)
+        val tabs = listOf(
+            TabItem(R.drawable.ic_nav_nutrition, getString(R.string.nav_nutrition)),
+            TabItem(R.drawable.ic_nav_record, getString(R.string.nav_record)),
+            TabItem(R.drawable.ic_nav_profile, getString(R.string.nav_profile))
+        )
+        binding.navBar.setTabs(tabs)
+        binding.navBar.restore("main_nav", 0)
+        binding.navBar.listener = { idx, _ -> switchTab(idx) }
+        switchTab(binding.navBar.selectedIndex)
+
         checkFirstLaunch()
     }
 
@@ -60,48 +66,6 @@ class MainActivity : BaseActivity() {
             .show()
     }
 
-    private fun setupNavSizing() {
-        val screenHeight = resources.displayMetrics.heightPixels
-        val density = resources.displayMetrics.density
-        val barHeightDp = (screenHeight * NavConfig.BOTTOM_BAR_HEIGHT_FRACTION / density).toInt()
-
-        val barParams = binding.navBar.layoutParams
-        barParams.height = dpToPxF(barHeightDp.toFloat()).toInt()
-        binding.navBar.layoutParams = barParams
-
-        val navItemIds = listOf(
-            binding.navNutrition.id,
-            binding.navRecord.id,
-            binding.navProfile.id
-        )
-        val navItems = navItemIds.map { findViewById<android.view.View>(it) }
-        val isOdd = navItems.size % 2 == 1
-
-        if (isOdd) {
-            val centerIndex = navItems.size / 2
-            val centerItem = navItems[centerIndex]
-
-            val sidePadding = 6f + 8f
-            val sideContentHeight = 24f + 4f + 14f + sidePadding
-            val centerContentHeight = 52f + 2f + 14f
-
-            val sideTextY = (barHeightDp - sideContentHeight) / 2f + 6f + 24f + 4f
-            val centerTextY = (barHeightDp - centerContentHeight) / 2f + 52f + 2f
-
-            centerItem.translationY = dpToPxF(sideTextY - centerTextY)
-
-            val params = centerItem.layoutParams
-            params.height = dpToPxF((barHeightDp * NavConfig.CENTER_BUTTON_SCALE).toFloat()).toInt()
-            centerItem.layoutParams = params
-        }
-    }
-
-    private fun setupNavClickListeners() {
-        binding.navNutrition.setOnClickListener { switchTab(0) }
-        binding.navRecord.setOnClickListener { switchTab(1) }
-        binding.navProfile.setOnClickListener { switchTab(2) }
-    }
-
     private fun switchTab(index: Int) {
         if (index == currentIndex) return
         currentIndex = index
@@ -109,55 +73,5 @@ class MainActivity : BaseActivity() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_container, fragments[index])
             .commit()
-
-        updateNavVisuals(index)
     }
-
-    private fun updateNavVisuals(selectedIndex: Int) {
-        val navIds = listOf(R.id.nav_nutrition, R.id.nav_record, R.id.nav_profile)
-        for (i in navIds.indices) {
-            val nav = findViewById<android.view.View>(navIds[i])
-            nav.isSelected = (i == selectedIndex)
-        }
-
-        val icons = listOf(
-            binding.navNutritionIcon,
-            binding.navRecordIcon,
-            binding.navProfileIcon
-        )
-        val labels = listOf(
-            binding.navNutritionLabel,
-            binding.navRecordLabel,
-            binding.navProfileLabel
-        )
-
-        val selectedColor = ContextCompat.getColor(this, R.color.primary)
-        val defaultColor = ContextCompat.getColor(this, R.color.on_surface_variant)
-
-        icons.forEachIndexed { i, icon ->
-            icon.setColorFilter(if (i == selectedIndex) selectedColor else defaultColor)
-        }
-
-        labels.forEachIndexed { i, label ->
-            label.setTextColor(if (i == selectedIndex) selectedColor else defaultColor)
-            label.setTypeface(null, if (i == selectedIndex) Typeface.BOLD else Typeface.NORMAL)
-            label.setTextSize(
-                android.util.TypedValue.COMPLEX_UNIT_PX,
-                resources.getDimension(R.dimen.text_size_micro)
-            )
-        }
-
-        updateCenterIconBackground(selectedIndex)
-    }
-
-    private fun updateCenterIconBackground(index: Int) {
-        val icon = binding.navRecordIcon
-        icon.background = if (index == 1) {
-            ContextCompat.getDrawable(this, R.drawable.bg_nav_center_selected)
-        } else {
-            ContextCompat.getDrawable(this, R.drawable.bg_nav_center)
-        }
-    }
-
-    private fun dpToPxF(dp: Float): Float = dp * resources.displayMetrics.density
 }
--- app/src/main/java/com/woshiwangnima/healthdietpro/config/NavConfig.kt
+++ app/src/main/java/com/woshiwangnima/healthdietpro/config/NavConfig.kt
@@ -2,7 +2,6 @@ package com.woshiwangnima.healthdietpro.config
 
 object NavConfig {
     const val BOTTOM_BAR_HEIGHT_FRACTION = 0.08f
-    const val CENTER_BUTTON_SCALE = 1.2f
 
     fun calculateBarHeightPx(screenHeightPx: Int, density: Float): Int =
         (screenHeightPx * BOTTOM_BAR_HEIGHT_FRACTION / density * density).toInt()
